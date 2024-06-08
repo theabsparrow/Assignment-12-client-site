a1e9b1a31b528568f0e93f14dc1dccf98d1e7e5f
theabsparrow <absbashar04@gmail.com>
2024-06-08 19:25:01 +0600

    profile done

--- src/components/banner/Slider.jsx
+++ src/components/banner/Slider.jsx
@@ -25,23 +25,29 @@ const Slider = ({ firstSixCards }) => {
             >
                 {
                     firstSixCards.map(sixCard => <SwiperSlide key={sixCard._id}>
-                        <div className='felx flex-col items-center justify-center mt-20 h-[70vh]'>
-                            <div className='text-center'>
-                                <h1 className='text-white font-xl'>category: <span>{sixCard.category}</span></h1>
+                        <div className='felx flex-col items-center justify-center mt-10 h-[70vh]'>
+                            <div className='text-center flex justify-center'>
+                                <h1 className='text-white font-xl bg-[#DED66B69] px-3 py-2 rounded-lg'>category: <span>{sixCard.category}</span></h1>
                             </div>
-                            <div className='text-center space-y-6 mt-4'>
-                                <h1 className='text-6xl font-bold text-white' >{sixCard.title}</h1>
-                                <p className='text-white font-semibold text-xl'>{sixCard.description}</p>
+                            <div className='flex justify-center'>
+                                <div className='text-center space-y-6 mt-4 bg-[#D7D5DC69]  px-6 py-3 rounded-xl'>
+                                    <h1 className='text-6xl font-bold text-white' >{sixCard.title}</h1>
+                                    <p className='text-white font-semibold text-xl'>{sixCard.description}</p>
+                                </div>
                             </div>
-                            <div className='text-white text-center mt-10 space-y-5'>
-                                <h1> Total question in this Survey: {sixCard.questions.length}</h1>
-                                {
-                                    sixCard.questions.map((question, i) => <h1 className='text-lg font-medium' key={i}>{question.description}</h1> )
-                                    
-                                }
+                            <div className='flex justify-center mt-4'>
+                                <h1 className='text-lg font-medium bg-[#D7D5DC69] px-3 py-2 rounded-xl text-white'> Total question in this Survey: {sixCard.questions.length}</h1>
+                            </div>
+                            <div className='flex justify-center'>
+                                <div className='text-white text-center mt-5 space-y-5 bg-[#D7D5DC69] px-5 py-3 rounded-xl'>
+                                    {
+                                        sixCard.questions.map((question, i) => <h1 className='text-lg font-medium' key={i}>{question.description}</h1>)
+
+                                    }
+                                </div>
                             </div>
                             <div className='flex justify-center mt-5'>
-                                <Link className='text-white bg-[#859770] px-2 py-2 rounded-xl'>Explore More to Participate</Link>
+                                <Link to={`/survey/${sixCard._id}`} className='text-white bg-[#859770] px-2 py-2 rounded-xl'>Explore More to Participate</Link>
                             </div>
                         </div>
                     </SwiperSlide>)
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet";
 import useAuth from "../hooks/useAuth";
+import cover from '../../public/images/Cover.png';
 
 
 const Profile = () => {
@@ -16,19 +17,19 @@ const Profile = () => {
                 <div className='bg-white shadow-lg rounded-2xl w-3/5'>
                     <img
                         alt='profile'
-                        src='https://wallpapercave.com/wp/wp10784415.jpg'
+                        src={cover}
                         className='w-full mb-4 rounded-t-lg h-36'
                     />
                     <div className='flex flex-col items-center justify-center p-4 -mt-16'>
                         <a href='#' className='relative block'>
                             <img
                                 alt='profile'
-                                src={user.photoURL}
+                                src={user?.photoURL}
                                 className='mx-auto object-cover rounded-full h-24 w-24  border-2 border-white '
                             />
                         </a>
 
-                        <p className='p-2 px-4 text-xs text-white bg-pink-500 rounded-full'>
+                        <p className='p-2 px-4 text-xs text-white bg-[#859770] rounded-full'>
                             Admin
                         </p>
                         <p className='mt-2 text-xl font-medium text-gray-800 '>
@@ -48,10 +49,10 @@ const Profile = () => {
                                 </p>
 
                                 <div>
-                                    <button className='bg-[#F43F5E] px-10 py-1 rounded-lg text-white cursor-pointer hover:bg-[#af4053] block mb-1'>
+                                    <button className='bg-[#859770] px-10 py-1 rounded-lg text-white cursor-pointer hover:bg-[#af4053] block mb-1'>
                                         Update Profile
                                     </button>
-                                    <button className='bg-[#F43F5E] px-7 py-1 rounded-lg text-white cursor-pointer hover:bg-[#af4053]'>
+                                    <button className='bg-[#859770] px-7 py-1 rounded-lg text-white cursor-pointer hover:bg-[#af4053]'>
                                         Change Password
                                     </button>
                                 </div>
